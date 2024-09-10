b1ac2569a1df47b324fc809f73d686bf37e5afad
Sam Allen <164280939+samallenonline@users.noreply.github.com>
2024-09-10 02:53:39 +0000

    Created samsMain.java, testing

---
+++ samsMain.java
@@ -0,0 +1,9 @@
+// CS3300 Software Development Project: LocalWave
+// Sam's branch 
+
+public class samsMain {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello Sam!");
+    }
+}
