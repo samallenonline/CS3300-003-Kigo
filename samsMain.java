da19c508bb54fdee58268af8b0b4f6e98ca3ff0a
Sam Allen <164280939+samallenonline@users.noreply.github.com>
2024-11-14 23:15:25 -0700

    update 11/14/24
    first large increment for front-end development
    - basic functionality
    - interface layout and navigation menus
    - GUI and design
    ** needs to be integrated with back-end development to move forward

--- samsMain.java
+++
@@ -1,9 +0,0 @@
-// CS3300 Software Development Project: LocalWave
-// Sam's branch 
-
-public class samsMain {
-    public static void main(String[] args) 
-    {
-        System.out.println("Hello Sam!");
-    }
-}
